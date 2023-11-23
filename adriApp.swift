eae063121f81e985ca3f028d08292c05656c3eb4
adrpr97 <130666088+adrpr97@users.noreply.github.com>
2023-11-23 11:46:57 +0100

    Initial commit

---
+++ adriApp.swift
@@ -0,0 +1,17 @@
+//
+//  adriApp.swift
+//  adri
+//
+//  Created by Adriano Prota on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct adriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
